8c0d53ea58a307d7f22e7b58adaba1b0eb1531c8
Thiago Porto <thiagoports@hotmail.com>
2025-07-23 16:33:13 -0300

    feat: adicionando projeto

---
+++ clientes/jairomotos/linktree/js/scripts.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const btnWhatsApp = document.querySelector(".btn-whatsapp");
+
+  if (btnWhatsApp) {
+    btnWhatsApp.classList.add("pulsar");
+  }
+});
