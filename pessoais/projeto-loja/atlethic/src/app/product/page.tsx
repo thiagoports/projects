a945ec9819b34c428d4c5e76b8168ad81a825733
Thiago <thiagoports@hotmail.com>
2025-09-09 08:23:47 -0300

    update: moto-mes

--- pessoais/projeto-loja/atlethic/src/app/product/page.tsx
+++
@@ -1,9 +0,0 @@
-const ProductPage = () => {
-  return (
-    <div>
-      <h1>Product Page</h1>
-    </div>
-  );
-};
-
-export default ProductPage;
---
+++ pessoais/projeto-loja/loja-online/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className={cn("text-red-500")}>Home</h1>
+      <Button>Thiago</Button>
+    </div>
+  );
+};
+
+export default Home;
